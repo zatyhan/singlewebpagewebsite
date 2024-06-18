56dade6ddf3fed2dea2be5a49b33153c4dd7ad96
zatyhan <nureizzaty@gmail.com>
2024-06-19 00:04:29 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup(card) {
+    document.getElementsByClassName('profile')[0].style.visibility= 'visible';
+    console.log(String(card));
+    // popup.style.visibility = 'visible';
+}
+
+// function foo(){
+//     console.log('hello');
+// }
