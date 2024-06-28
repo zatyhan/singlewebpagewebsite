f7b5bb4cd3bba7c3a1827accfe5363169a61b916
zatyhan <nureizzaty@gmail.com>
2024-06-28 03:28:41 +0100

    profile popup update

--- script.js
+++
@@ -1,9 +0,0 @@
-function showPopup(card) {
-    document.getElementsByClassName('profile')[0].style.visibility= 'visible';
-    console.log(String(card));
-    // popup.style.visibility = 'visible';
-}
-
-// function foo(){
-//     console.log('hello');
-// }
