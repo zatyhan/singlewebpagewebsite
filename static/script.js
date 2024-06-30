790e1f393f86cafed3d9eb694bbf3d5172d871c3
zatyhan <nureizzaty@gmail.com>
2024-06-30 02:23:49 +0100

    education popup animation

--- static/script.js
+++ static/script.js
@@ -32,11 +32,42 @@ function typeWriter()
     //         currentText='';
     // }
 }
-        
-        
-        
+
+
+
 $(document).ready(function(){
+    var verticalCircleAnime = anime({
+        targets: '.vertical-line',
+        height: ['0px', '50rem'],
+        easing: 'linear',
+        duration: 3000,
+        repeat: 1
+        // loop: true,
+    });
+
+    var educationAnime = anime.timeline({
+        easing: 'linear',
+        duration: 3000,
+        backgroundColor: '#f1c40f',
+    });
+    educationAnime.add({
+        targets: '.hs1',
+        height: ['0px', '100px'],
+        duration: 1000,
+        easing: 'easeOutQuart'
+    }).add({
+        targets: '.college',
+        height: ['0px', '250px'],
+        duration: 1000,
+        easing: 'easeOutQuart'
+    }).add({
+        targets: '.university',
+        height: ['0px', '400px'],
+        duration: 1000,
+        easing: 'easeOutQuart'
+    });
     $('.popup').hide();
+    $('.popup.education').show();
     typeWriter();
     // setInterval(()=>{
     //     typeWriter();
@@ -47,6 +78,7 @@ $(document).ready(function(){
     //     2000);
     // }, 5000);
 
+
     $('.card').hover(function(){
         $(this).css('transform' , 'scale(1.1)');
     }, function(){
@@ -54,11 +86,27 @@ $(document).ready(function(){
     });
 
     $('.card').click(function(){
-        console.log('.popup .'+$(this).attr("id"));
+        console.log($(this).attr("id"));
         $('.popup.'+$(this).attr("id")).show();
+        if ($(this).attr("id")=="education")
+        {
+            verticalCircleAnime.restart();
+            // $('.edbox.hs1').slideDown();
+            // $('.edbox.college').slideDown();
+            // $('.edbox.university').slideDown();
+            educationAnime.restart();
+            // educationAnime.seek(0);
+        }
+        
     });
-
+    
     $('.close-container').click(function(){
+        console.log($(this).parent().attr("class"));
         $($(this).parent()).hide();
+        if ($(this).attr("class")=="education popup")
+        {
+            verticalCircleAnime.seek(0);
+            educationAnime.seek(0);
+        }
     });
     });
