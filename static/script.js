bc7945bf322ad04ec5b01779029473e4343b2ff5
zatyhan <nureizzaty@gmail.com>
2024-06-30 03:33:56 +0100

    repositioned elements in edu popup

--- static/script.js
+++ static/script.js
@@ -38,7 +38,7 @@ function typeWriter()
 $(document).ready(function(){
     var verticalCircleAnime = anime({
         targets: '.vertical-line',
-        height: ['0px', '50rem'],
+        height: ['0px', '60rem'],
         easing: 'linear',
         duration: 3000,
         repeat: 1
@@ -48,24 +48,50 @@ $(document).ready(function(){
     var educationAnime = anime.timeline({
         easing: 'linear',
         duration: 3000,
-        backgroundColor: '#f1c40f',
+        // backgroundColor: '#f1c40f',
     });
     educationAnime.add({
         targets: '.hs1',
-        height: ['0px', '100px'],
-        duration: 1000,
+        height: ['0px', '100%'],
+        duration: 700,
         easing: 'easeOutQuart'
     }).add({
         targets: '.college',
-        height: ['0px', '250px'],
-        duration: 1000,
+        height: ['0px', '100%'],
+        // height: ['0px', '250px'],
+        duration: 1300,
         easing: 'easeOutQuart'
     }).add({
         targets: '.university',
-        height: ['0px', '400px'],
-        duration: 1000,
+        height: ['0px', '100%'],
+        // height: ['0px', '500px'],
+        duration: 2000,
         easing: 'easeOutQuart'
     });
+    var yearAnime = anime.timeline({
+        easing: 'linear',
+        duration: 3000,
+        backgroundColor: '#f1c40f',
+    });
+    yearAnime.add({
+        targets: document.querySelector('._2016 > .year'),
+        height: ['0px', '1em'],
+        duration: 700,
+        easing: 'easeOutQuart'
+    }).add({
+        targets: document.querySelector('._2017 > .year'),
+        height: ['0px', '1em'],
+        // height: ['0px', '250px'],
+        duration: 1300,
+        easing: 'easeOutQuart'
+    }).add({
+        targets: document.querySelector('._2019 > .year'),
+        height: ['0px', '1em'],
+        // height: ['0px', '500px'],
+        duration: 2000,
+        easing: 'easeOutQuart'
+    });
+
     $('.popup').hide();
     $('.popup.education').show();
     typeWriter();
@@ -91,11 +117,8 @@ $(document).ready(function(){
         if ($(this).attr("id")=="education")
         {
             verticalCircleAnime.restart();
-            // $('.edbox.hs1').slideDown();
-            // $('.edbox.college').slideDown();
-            // $('.edbox.university').slideDown();
             educationAnime.restart();
-            // educationAnime.seek(0);
+            yearAnime.restart();
         }
         
     });
@@ -104,9 +127,10 @@ $(document).ready(function(){
         console.log($(this).parent().attr("class"));
         $($(this).parent()).hide();
         if ($(this).attr("class")=="education popup")
-        {
-            verticalCircleAnime.seek(0);
-            educationAnime.seek(0);
+            {
+                verticalCircleAnime.seek(0);
+                educationAnime.seek(0);
+                yearAnime.seek(0);
         }
     });
     });
